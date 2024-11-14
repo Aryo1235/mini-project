44ce7bcbed66b9150cc09e315a74ab1b96c7a532
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2024-11-14 20:40:28 +0700

    Update App.jsx

--- energy-management-app/src/App.jsx
+++ energy-management-app/src/App.jsx
@@ -5,8 +5,10 @@ import AddEnergyForm from "./components/AddEnergyForm";
 import EditEnergyForm from "./components/EditEnergyForm";
 import EnergyDetail from "./components/EnergyDetail";
 import LandingPage from "./pages/LandingPage";
+import Navbars from "./components/Navbar";
 import Login from "./pages/Login";
 import LoginUser from "./pages/LoginUser";
+import ChatBot from "./components/Chat-Bot";
 
 import ProtectedRoute from "./utils/ProtectedRoutes"; // Tambahkan ProtectedRoute
 import NavbarsSupa from "./components/NavbarSupa";
@@ -17,19 +19,8 @@ function App() {
       <Router>
         <NavbarsSupa />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/loginuser" element={<LoginUser />} />
-
           {/* Protected Routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/home" element={<EnergyList />} />
-            <Route path="/home/:id" element={<EnergyDetail />} />
-            <Route path="/add" element={<AddEnergyForm />} />
-            <Route path="/edit/:id" element={<EditEnergyForm />} />
-
-            {/* Route untuk halaman chatai */}
-          </Route>
+          <Route element={<ProtectedRoute />}></Route>
         </Routes>
       </Router>
     </Flowbite>
