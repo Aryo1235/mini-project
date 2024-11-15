91478d8ede427c37469bceb87f11500884dbbee6
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2024-11-15 14:10:03 +0700

    Update HeroSection.jsx

--- energy-management-app/src/components/HeroSection.jsx
+++ energy-management-app/src/components/HeroSection.jsx
@@ -5,23 +5,25 @@ export default function HeroSection() {
     <div>
       {/* Hero */}
       <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
-        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+        <div className="max-w-[100rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
           {/* End Announcement Banner */}
           {/* Title */}
-          <div className="mt-5 max-w-2xl text-center mx-auto">
+          <div className="mt-5 max-w-3xl text-center mx-auto">
             <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-neutral-200">
-              Lets Build
+              Kelola Listrikmu{" "}
               <span className="bg-clip-text bg-gradient-to-tl from-green-600 to-green-400 text-transparent">
-                Together
+                Eco-Watt
               </span>
             </h1>
           </div>
           {/* End Title */}
           <div className="mt-5 max-w-3xl text-center mx-auto">
             <p className="text-lg text-gray-600 dark:text-neutral-400">
-              Preline UI is an open-source set of prebuilt UI components,
-              ready-to-use examples and Figma design system based on the
-              utility-first Tailwind CSS framework.
+              Eco-Watt adalah platform manajemen energi listrik yang dirancang
+              untuk membantu Anda memahami, mengelola, dan mengoptimalkan
+              konsumsi energi dengan lebih cerdas.Eco-Watt mendukung Anda dalam
+              mengurangi biaya listrik sekaligus menjaga keberlanjutan
+              lingkungan.
             </p>
           </div>
           {/* Buttons */}
