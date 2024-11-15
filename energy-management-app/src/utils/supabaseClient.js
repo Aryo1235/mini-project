8a2604d017a5761b3e073434c94c7c1b0a37ea57
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2024-11-15 18:44:21 +0700

    Update supabaseClient.js

--- energy-management-app/src/utils/supabaseClient.js
+++ energy-management-app/src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sbwxeegxwgafmazqabsq.supabase.co"; // Ganti dengan URL proyek Anda
+const supabaseUrl = "https://sbwxeegxwgafmazqabsq.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNid3hlZWd4d2dhZm1henFhYnNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5Njg2MDIsImV4cCI6MjA0NjU0NDYwMn0.-s8yNEH88VYyRlI1WC6ED4UfkEY6J9kTi9lFjgH6oUM"; // Ganti dengan anon public key proyek Anda
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNid3hlZWd4d2dhZm1henFhYnNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5Njg2MDIsImV4cCI6MjA0NjU0NDYwMn0.-s8yNEH88VYyRlI1WC6ED4UfkEY6J9kTi9lFjgH6oUM";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
