6f5e92173d81daa1a20eeec7fd8826dc6f9c8f51
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2024-11-19 15:00:17 +0700

    Update energyApi.js

--- energy-management-app/src/utils/ServiceApi/energyApi.js
+++ energy-management-app/src/utils/ServiceApi/energyApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://6718aacc7fc4c5ff8f4a7ab2.mockapi.io/energy",
+const instance = axios.create({
+  baseURL: "https://6718aacc7fc4c5ff8f4a7ab2.mockinstance.io/energy",
 });
 
-export const getEnergyData = () => api.get("/");
-export const getEnergyDataById = (id) => api.get(`/${id}`);
-export const addEnergyData = (data) => api.post("/", data);
-export const updateEnergyData = (id, data) => api.put(`/${id}`, data);
-export const deleteEnergyData = (id) => api.delete(`/${id}`);
+export const getEnergyData = () => instance.get("/");
+export const getEnergyDataById = (id) => instance.get(`/${id}`);
+export const addEnergyData = (data) => instance.post("/", data);
+export const updateEnergyData = (id, data) => instance.put(`/${id}`, data);
+export const deleteEnergyData = (id) => instance.delete(`/${id}`);
